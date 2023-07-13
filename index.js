a259dd75ae28e45d37d555f930637fa88e77bfcd
MARY NJOKI <marynjoki738@gmail.com>
2023-07-13 18:31:34 +0300

    scope lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+
+  const leastFavoriteCustomer = 'someone';
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another person';
+  }
